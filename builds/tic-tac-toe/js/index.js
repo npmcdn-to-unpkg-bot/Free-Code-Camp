b8d61298e0107128c169386ebea7fcf31846a2c0
Joe Chimienti <jchimien@mail.usf.edu>
2016-04-23 19:44:23 -0400

    changed alerts to bootstrap modals

--- builds/tic-tac-toe/js/index.js
+++ builds/tic-tac-toe/js/index.js
@@ -1,9 +1,20 @@
-//problem with second game computer starts every game.....
-
-var turn = "";
-$("td").click(place);
+var numberGames = 1;
+var turn;
 var winningPlayer;
+var choice;
+var coin;
+var turnGlyphicon;
+var firstPlayer;
+var computerWins=0;
+var playerWins=0;
+var ties=0;
 
+$(document).ready(function() {
+  $("#numberGames").html("Game #: " + numberGames);
+  $("#losses").html("Losses: "+computerWins);
+  $("#wins").html("Wins: "+ playerWins);
+  $("#ties").html("Ties: "+ ties);
+});
 var model = {
   board: ["", "", "", "", "", "", "", "", ""],
   wins: [
@@ -19,42 +30,67 @@ var model = {
 
 };
 
-$(document).ready(whoGoesFirst);
+$("#choiceForm").click(function() {
+  choice = $(" #choiceForm input[ type='radio']:checked").val();
+  console.log("choice: " + choice);
 
-function whoGoesFirst() {
-  if (winningPlayer === undefined) {
-    choice = prompt("choose 1 or 2 to decide who goes first");
-    choice = +choice;
-    coin = Math.floor(Math.random() * 2) + 1;
-    if (choice !== 1 && choice !== 2) {
-      alert("Pick 1 or 2 ");
-      choice = prompt("choose 1 or 2 to decide who goes first");
-    }
-    if (choice === coin) {
+});
 
-      alert("Go first");
-      turn = "player";
-    } else {
-      alert("Computer Goes First");
-      turn = "computer";
-      computerPlay();
+$("#startGame").click(function() {
 
-    }
-
+  whoGoesFirst();
+  $("#modalHeader").empty();
+  $("#startModalBody").html("<h2>Turn: " + turnGlyphicon + "</h2><br><h4>Go First!</h4>");
+  if (turn === "computer") {
+    $("#startModalBody").html("<h2>The" + turn.charAt(0).toUpperCase() + " " + turn.substr(1) + " will go first<h2>");
+  } else {
+    $("#startModalBody").html("<h2>Go First! " + turnGlyphicon + "</h2>");
   }
 
+  $("#startModal").modal('show');
+
+});
+
+$("td").click(place);
+
+function whoGoesFirst() {
+  coin = Math.floor(Math.random() * 2 + 1);
+  coin = 2; // hardcoded coin to 2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  console.log("coin: " + coin);
+  console.log("choice: " + choice);
+  console.log("does coin = choice? :");
+  console.log(coin == choice);
+
+  if (choice === undefined) {
+    return;
+  }
+  if (choice == coin) {
+    turnGlyphicon = "<h2> Player <span class='glyphicon glyphicon-user'><span></h2>";
+    turn = "player";
+
+  } else {
+    turnGlyphicon = "<h2> Computer <span class='glyphicon glyphicon-king'></span></h2>";
+    turn = "computer";
+    setTimeout(computerPlay, 4000);
+
+  }
+  console.log("turn: " + turn);
+  firstPlayer = turn;
+  $("#displayMe").append(firstPlayer.charAt(0).toUpperCase() + turn.substr(1));
+
 }
+
 // place player pick on board
 function place() {
-  console.log(turn);
+
   var td = $(this);
-  var $location = $(this).data("location");
-  console.log($location);
+  var $location = td.data("location");
+  console.log("placement location: " + $location);
   if (turn === "player") {
 
     if (model.board[$location] !== "") {
-      alert("spot already taken");
-      return;
+      return alert("spot already taken");
+
     } else {
       td.html("X");
       model.board[$location] = "X";
@@ -72,6 +108,8 @@ function place() {
 
 function setNextPlayer() {
   console.log("turn before changing: " + turn);
+  $("#displayMe").removeClass("glyphicon");
+
   if (turn === "player") {
     turn = "computer";
     console.log(" Turn after changing: " + turn)
@@ -81,6 +119,7 @@ function setNextPlayer() {
     console.log(" Turn after changing: " + turn)
     return;
   }
+  $("#displayMe").html("Turn: " + turnGlyphicon);
 
 }
 
@@ -91,13 +130,26 @@ function checkWin() {
     var pattern = model.wins[i];
     var player = model.board[pattern[0]] + model.board[pattern[1]] + model.board[pattern[2]];
     if (player === "XXX") {
-      alert("You've won!");
+      
       winningPlayer = "player";
-      resets();
+      playerWins++;
+      $("#wins").html("Wins: "+playerWins);
+       $("#modalHeader").html("<h4>game winner:</h4>");
+  $("#startModalBody").html("<h2>" + winningPlayer + " won the game!</h2>");
+  $("#startModal").modal('show');
+  $("#startModal").fadeOut(3000);
+      setTimeout(resets,1500);
     } else if (player === "OOO") {
-      alert("Computer Won");
+     
       winningPlayer = "computer";
-      resets();
+      computerWins++;
+     
+      $("#losses").html("Losses: "+computerWins);
+       $("#modalHeader").html("<h4>game winner:</h4>");
+  $("#startModalBody").html("<h2>" + winningPlayer + " won the game!</h2>");
+  $("#startModal").modal('show');
+  $("#startModal").fadeOut(3000);
+      setTimeout(resets,1500);
     }
 
   }
@@ -109,8 +161,12 @@ function checkWin() {
     }
   });
   if (wins) {
-    alert("Tie game");
-    resets();
+    ties++;
+    $("#ties").html("Ties: "+ties);
+   $("#modalHeader").empty();
+    $("#startModalBody").html("<h2>Tie Game!</h2>");
+    $("#startModal").modal('show');
+    setTimeout(resets,1500);
 
   }
 
@@ -135,62 +191,63 @@ function xoValuesDisplay() {
 }
 // computer goes for win if available then blocks, then goes for it's preferred play
 function computerPlay() {
+  if (turn === "computer") {
 
-  xoValuesDisplay();
+    xoValuesDisplay();
 
-  var oArray = [];
-  var oPlacement = [];
-  for (j in model.wins) {
-    oArray = [];
-    model.wins[j].filter(function(item, index) {
-      if (oValues.indexOf(item) !== -1) {
-        oArray.push(item);
-      } else {
-        oPlacement = item;
+    var oArray = [];
+    var oPlacement = [];
+    for (j in model.wins) {
+      oArray = [];
+      model.wins[j].filter(function(item, index) {
+        if (oValues.indexOf(item) !== -1) {
+          oArray.push(item);
+        } else {
+          oPlacement = item;
+        }
+      });
+      // win the game if available
+      if (oArray.length === 2 && model.board[oPlacement] === "") {
+        model.board[oPlacement] = "O";
+        $('[data-location=' + oPlacement + ']').html("O");
+        checkWin();
+
+        return;
       }
-    });
-    // win the game if available
-    if (oArray.length === 2 && model.board[oPlacement] === "") {
-      model.board[oPlacement] = "O";
-      $('[data-location=' + oPlacement + ']').html("O");
-      checkWin();
 
-      return;
     }
+    //block if player has 2 in a row and no win available
+    for (k in model.wins) {
+      var checkArr = [];
+      placementArr = [];
+      model.wins[k].filter(function(item, index) {
+        if (xValues.indexOf(item) !== -1) {
+          checkArr.push(item);
+        } else {
+          placementArr.push(item);
+        }
+      });
 
-  }
-  //block if player has 2 in a row and no win available
-  for (k in model.wins) {
-    var checkArr = [];
-    placementArr = [];
-    model.wins[k].filter(function(item, index) {
-      if (xValues.indexOf(item) !== -1) {
-        checkArr.push(item);
-      } else {
-        placementArr.push(item);
-      }
-    });
-
-    if (checkArr.length === 2 && model.board[placementArr] === "") {
-      model.board[placementArr] = "O";
-      $('[data-location=' + placementArr + ']').html("O");
-      /*
+      if (checkArr.length === 2 && model.board[placementArr] === "") {
+        model.board[placementArr] = "O";
+        $('[data-location=' + placementArr + ']').html("O");
+        /*
       console.log(model.wins[k]);
       console.log(model.board);
       console.log(checkArr);
       console.log(placementArr);
 	  */
-      checkWin();
-      setNextPlayer();
-      return;
+        checkWin();
+        setNextPlayer();
+        return;
+
+      }
 
     }
 
+    AI();
+    return;
   }
-
-  AI();
-  return;
-
 }
 
 function AI() {
@@ -198,11 +255,11 @@ function AI() {
     var preferredPlay = [4, 0, 8, 5, 1, 7, 2, 6, 3];
     for (j in preferredPlay) {
       var currentTile = preferredPlay[j];
-      console.log(currentTile);
+      console.log("computer plays at location: " + currentTile);
       if (model.board[currentTile] === "") {
         $('[data-location=' + currentTile + ']').html("O");
         model.board[currentTile] = "O"
-        console.log(model.board);
+        console.log("board: " + model.board);
         checkWin();
         console.log("set next Player");
         setNextPlayer();
@@ -217,7 +274,10 @@ function AI() {
 }
 // reset game 
 function resets() {
-  alert("Next Game!");
+ 
+
+  numberGames++;
+  $("#numberGames").html("Game #: " + numberGames)
   model.board = ["", "", "", "", "", "", "", "", ""];
   model.win = false;
   $("td").each(function() {
@@ -236,11 +296,7 @@ function resets() {
   }
   //if tie game
   else if (winningPlayer === undefined) {
-    if (coin === choice) {
-      turn = "player";
-    } else {
-      turn = "computer";
-    }
+    turn = firstPlayer;
   }
-
+  $("#displayMe").html("First Player: " + turn.charAt(0).toUpperCase()+turn.substr(1));
 }
