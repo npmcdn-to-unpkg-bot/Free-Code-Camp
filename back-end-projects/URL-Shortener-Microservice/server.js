62d61bc72c661e2edff5cbac4dfaf321f46ee119
joe <jchimien@mail.usf.edu>
2016-08-21 12:50:58 +0000

    test backend

--- back-end-projects/URL-Shortener-Microservice/server.js
+++ back-end-projects/URL-Shortener-Microservice/server.js
@@ -7,22 +7,26 @@ var path = require('path'),
 	//To get the data submitted in the body of the POST request, we will use a middleware called body-parser, we can install it via NPM:
 	bodyParser = require('body-parser');
 	app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 var db;
 var coll;
-MongoClient.connect('mongodb://joechimienti:<password>@ds031915.mlab.com:31915/hello-world', function(err, database){
-	if (err) throw err
+MongoClient.connect('mongodb://joechimienti:<pass>@ds031915.mlab.com:31915/hello-world', function(err, database){
+	if (err) throw err;
 	db = database;
-// coll = db.collection('shortenURL')
+// coll = db.collection('shortenURL') // could not connect to coll
 	app.listen(port, function(){
 		console.log('mongo ' + port);
 	});
-	//db.close();
 });
 app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname, 'public', 'index.html'))
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+	res.end();
+});
+app.get('/url/*', function(req, res){
+	var userURL = req.url.substr();
+	coll.findOne({short: userURL});
+	res.redirect();
 })
-
 app.post('/*', function(req, res){
 	// get req.url from form
 	console.log(req.body.url);
@@ -36,7 +40,7 @@ app.post('/*', function(req, res){
 	    short: dec
    });
    db.close();
-   res.redirect('/');
+   res.send('shortened url is: http://www.example.com/' + dec);
    
-})
+});
 
