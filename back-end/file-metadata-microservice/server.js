7d6ff88822e424ffd7a0799d8d818bedf912f2c7
jchimienti89 <jchimien@mail.usf.edu>
2016-08-26 16:39:34 -0400

    update front end

---
+++ back-end/file-metadata-microservice/server.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+app = express(),
+multer = require('multer'),
+path = require('path'),
+upload = multer(),
+port = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.post('/upload', upload.single('file'), function(req, res){
+	if (req.file){
+		res.json({
+			'filename': req.file.originalname,
+			'size': req.file.size + ' B'
+		})
+	}else{
+		res.status(400).end();
+	}
+});
+app.listen(port, function(){
+	console.log('listening on port', port)
+});
