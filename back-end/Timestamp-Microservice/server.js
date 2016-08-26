9750cf040993c07bec6c17f72bd4d1ea8533134d
jchimienti89 <jchimien@mail.usf.edu>
2016-08-26 14:14:15 -0400

    timestamp date formatting for chromium loading

---
+++ back-end/Timestamp-Microservice/server.js
@@ -0,0 +1,46 @@
+var express = require('express'),
+app = express(),
+path = require('path'),
+moment = require('moment');
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+}); 
+app.get('/[a-z]|[A-Z]*', function(req, res){
+	var pathz = req.path.replace(/(st|rd|nd|th|%20)/g, ' ').substr(1);
+	//http://stackoverflow.com/questions/23263380/deprecation-warning-moment-construction-falls-back-to-js-date
+	var d = moment(pathz)
+	console.log(date);
+	if ( d.isValid()){
+		var unix = d.format('x')
+		var natural = d.format('MM-DD-YYYY')
+		var date = {
+			'natural': natural,
+			'unix': unix
+		};
+		 return res.send(date).end();
+	}else{
+			res.send('Invalid date format').end();
+	}
+	
+	
+	
+})
+app.get('/[0-9]*', function(req, res){
+	var p = Number(req.path.substr(1));
+	var d = moment(p)
+	if ( d.isValid()){
+		var unix = d.format('x')
+		var natural = d.format('MM-DD-YYYY')
+		var date = {
+			'natural': natural,
+			'unix': unix
+		};
+		 return res.send(date).end();
+	}else{
+			res.send('Invalid date format').end();
+	}
+});
+app.listen(8000, function(){
+	console.log('listening at port ', 8000)
+})
