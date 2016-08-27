54c68e0086f84d10e9cac29a4ef9e208b991b507
jchimienti89 <jchimien@mail.usf.edu>
2016-08-26 21:19:24 -0400

    deploy timestamp to heroku

--- back-end/Timestamp-Microservice/index.js
+++ back-end/Timestamp-Microservice/index.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 app = express(),
 path = require('path'),
-moment = require('moment');
+moment = require('moment'),
+port = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -9,7 +10,8 @@ app.get('/', function(req, res){
 app.get('/[a-z]|[A-Z]*', function(req, res){
 	var pathz = req.path.replace(/(st|rd|nd|th|%20)/g, ' ').substr(1);
 	//http://stackoverflow.com/questions/23263380/deprecation-warning-moment-construction-falls-back-to-js-date
-	var d = moment(pathz)
+	var datez = new Date(pathz);
+	var d = moment(datez)
 	console.log(date);
 	if ( d.isValid()){
 		var unix = d.format('x')
@@ -41,6 +43,6 @@ app.get('/[0-9]*', function(req, res){
 			res.send('Invalid date format').end();
 	}
 });
-app.listen(8000, function(){
-	console.log('listening at port ', 8000)
+app.listen(port, function(){
+	console.log('listening at port ', port)
 })
