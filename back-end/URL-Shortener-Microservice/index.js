95857ae23c51a507674f2b634560b4d04653f5ff
jchimienti89 <jchimien@mail.usf.edu>
2016-08-31 00:34:48 -0400

    url shortener index.js insert into db

--- back-end/URL-Shortener-Microservice/index.js
+++ back-end/URL-Shortener-Microservice/index.js
@@ -3,12 +3,14 @@ var mongo = require('mongodb').MongoClient,
 	app = express(),
 	path = require('path'),
 	port = process.env.PORT || 5000,
-	username = process.env.USERNAME,//require('./config.js').username,//process.env.username,
-	password = process.env.PASSWORD,//require('./config.js').password,//process.env.password,
+    helmet = require('helmet'),
+	username = process.env.USERNAME,//require('./config.js').username,
+	password = process.env.PASSWORD,//require('./config.js').password,
 	shorten = require('./shortenUrl.js'),
 	coll, 
 	 db;
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 mongo.connect('mongodb://' + username + ':' + password + '@ds031915.mlab.com:31915/hello-world',
    function(err, database){
 		if (err) throw err;
@@ -24,9 +26,9 @@ app.get('/', function(req, res){
 app.get('/favicon.ico', function(req, res){
 	console.log('favicon')
 });
-app.get('*[object%20HTMLInputElement]*', function(req, res){
-	console.log('html input', req.url)
-});
+
+
+
 app.get('/new/*', function( req, res){ 
 	console.log('in app.get/NEW', req.url)
 	
@@ -53,6 +55,14 @@ console.log('req.params.url', urlz.substr(8));
 	}
     db.close();
 });
+/*
+app.get('*[object%20HTMLInputElement]*', function(req, res, next){
+	console.log('html input', req.url)
+	//res.send('hello');
+
+});
+*/
+
 app.get('/:id', function(req, res){
 	console.log('in app.get :id', req.params.id);
 	var u = coll.find({"obj.short": req.params.id}).toArray(function(err, documents){
@@ -60,6 +70,7 @@ app.get('/:id', function(req, res){
 		console.log(a);
 		res.redirect(a);
 	});	
+	console.log(u);
 });
 app.listen(port, function(err){
 	if (err) throw err;
